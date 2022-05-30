fa03db62163612e49e844c6a9efb47e270171eaa
iRewiewer <irewiewer@gmail.com>
2022-05-30 02:52:17 -0400

    21) Did 1 more - 13

---
+++ Done/13/decoder.py
@@ -0,0 +1,4 @@
+import codecs
+
+rot13 = "cvpbPGS{abg_gbb_onq_bs_n_ceboyrz}"
+print(codecs.decode(rot13, 'rot_13'))
---
+++ Done/13/readme.txt
@@ -0,0 +1,2 @@
+# https://play.picoctf.org/practice/challenge/62
+# flag is 'picoCTF{not_too_bad_of_a_problem}'
